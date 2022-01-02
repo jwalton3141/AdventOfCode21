633253776d7a24553ba57822bcb79d516422c776
Jack Walton <jwalton3141@gmail.com>
2022-01-02 09:56:16 +0000

    Day 14

---
+++ day14/d14_utils.py
@@ -0,0 +1,21 @@
+def load_data():
+    """Read Day 14 puzzle input."""
+    return load_template(), load_rules()
+
+
+def load_template():
+    """Load polymer template into string."""
+    with open("d14_input_template.txt") as f:
+        template = f.read().splitlines()[0]
+    return template
+
+
+def load_rules():
+    """Load pair insertion rules into dictionary."""
+    with open("d14_input_rules.txt") as f:
+        rules = f.read().splitlines()
+    # Split at and remove arrow
+    rules = [rule.split(" -> ") for rule in rules]
+    # Create rule map
+    rules_map = {rule[0]: rule[1] for rule in rules}
+    return rules_map
