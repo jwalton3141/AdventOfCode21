52184eacc3d9ca9a1b4965e281eac0ea02aa2781
Jack Walton <jwalton3141@gmail.com>
2022-03-08 17:42:11 +0000

    Test solutions and lint code with GitHub workflow

--- day15/part1.py
+++ day15/part1.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python3
 
-import networkx as nx
-
 from utils import load_data, get_lowest_risk
 
 
---
+++ scripts/pytest.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -e
+
+for day in $(find -type f -name test_solutions.py -exec dirname {} + | sort); do
+    echo -e \\nTesting $(basename $day)...; \
+    cd $day && pytest ; \
+    cd ..; \
+done
